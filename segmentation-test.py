9d4b1f43e1be6f63ceb9a5a6f7be5e09cd8c3465
Siang417 <t113C52034@ntut.edu.tw>
2025-07-22 11:23:50 +0800

    重建U-Net架構，移除對Get_Vein_CNNTrain.py的依賴關係

--- segmentation-test.py
+++ segmentation-test.py
@@ -8,7 +8,72 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from Get_Vein_CNNTrain import UNet
+
+# ---------------------------- U-Net模型 ----------------------------
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=2):
+        super().__init__()
+        self.down1 = DoubleConv(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+        self.down2 = DoubleConv(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+        self.down3 = DoubleConv(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+        self.down4 = DoubleConv(256, 512)
+        self.pool4 = nn.MaxPool2d(2)
+
+        self.bottleneck = DoubleConv(512, 1024)
+
+        self.up4 = nn.ConvTranspose2d(1024, 512, 2, stride=2)
+        self.dec4 = DoubleConv(1024, 512)
+        self.up3 = nn.ConvTranspose2d(512, 256, 2, stride=2)
+        self.dec3 = DoubleConv(512, 256)
+        self.up2 = nn.ConvTranspose2d(256, 128, 2, stride=2)
+        self.dec2 = DoubleConv(256, 128)
+        self.up1 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.dec1 = DoubleConv(128, 64)
+
+        self.final_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        d1 = self.down1(x)
+        d2 = self.down2(self.pool1(d1))
+        d3 = self.down3(self.pool2(d2))
+        d4 = self.down4(self.pool3(d3))
+        bn = self.bottleneck(self.pool4(d4))
+
+        u4 = self.up4(bn)
+        u4 = torch.cat([u4, d4], dim=1)
+        u4 = self.dec4(u4)
+
+        u3 = self.up3(u4)
+        u3 = torch.cat([u3, d3], dim=1)
+        u3 = self.dec3(u3)
+
+        u2 = self.up2(u3)
+        u2 = torch.cat([u2, d2], dim=1)
+        u2 = self.dec2(u2)
+
+        u1 = self.up1(u2)
+        u1 = torch.cat([u1, d1], dim=1)
+        u1 = self.dec1(u1)
+
+        return self.final_conv(u1)
 
 # ---------------------------- 資料集 ----------------------------
 class VesselDatasetTest(Dataset):
@@ -143,7 +208,7 @@ def pixel_acc(pred, target):
 
 # ---------------------------- 測試執行器 ----------------------------
 def run_test(use_ground_truth=False):
-    TEST_IMG_DIR  = r"C:\Users\Zz423\Desktop\研究所\UCL\旺宏\Redina 資料\test"
+    TEST_IMG_DIR  = r"C:\Users\Zz423\Desktop\研究所\UCL\旺宏\Redina 資料\Quadrant_division\NoPlus1-OD"
     TEST_MASK_DIR = r"C:\Users\Zz423\Downloads\SEGMENTATION\FIVES\test\Ground Truth" if use_ground_truth else None
     CKPT_PATH     = r"D:\ROP\best_vessel_cnn.pth"
     SAVE_PRED_DIR_BASE = r"C:\Users\Zz423\Desktop\研究所\UCL\旺宏\Redina 資料\Predictions2"
